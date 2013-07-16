47a1b652ce4f55e8a55f4c3f33c8d07049294bac
Alfonsas Cirtautas <alfonsas.cirtautas@callcreditgroup.com>
2013-07-16 07:38:01 +0100

    Added Guass a Number

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Play(1, 10);
+        }
+
+        /**
+         * Play game Gues A Number.
+         **/
+        private static void Play(int From, int To)
+        {
+            int number = Think(From, To);
+            int guess = 0;
+            int tries = 0;
+
+            Console.WriteLine("Enter a number " + From + ".." + To);
+
+            do {
+
+                guess = Guess();
+                tries++;
+
+                if (number < guess)
+                {
+                    Console.WriteLine("try smaller");
+                }
+
+                if (number > guess)
+                {
+                    Console.WriteLine("try bigger");
+                }
+
+            } while (number != guess);
+
+            Console.WriteLine("You win! (" + tries + " tries)");
+            Console.ReadLine();
+        }
+
+        /**
+         * Think a number in range From To.
+         **/
+        private static int Think(int From, int To)
+        {
+            int number = new Random().Next(From, To);
+
+            return number;
+        }
+
+        /**
+         * Guess a number.
+         **/
+        private static int Guess()
+        {
+            int guess = 0;
+
+            guess = int.Parse(Console.ReadLine());
+
+            return guess;
+        }
+    }
+}
